23e4597a0209bb33b17a4ccdc6c2bb5be52fdf02
Leejun <estherleejunkim@gmail.com>
2025-10-09 14:43:46 +0900

    add mini js challenges

---
+++ js_challenge_1-2.js
@@ -0,0 +1,54 @@
+let castle = {
+  location: "Germany",
+  rented: false,
+};
+
+let person = {
+  name: "Leejun",
+  age: 23,
+  country: "South Korea",
+};
+
+function logData() {
+  let sentence =
+    person.name +
+    " is " +
+    person.age +
+    " years old and lives in " +
+    person.country;
+  console.log(sentence);
+}
+
+// logData();
+
+let age = 6;
+function ageCheck() {
+  if (age < 6) {
+    console.log("free");
+  } else if (age <= 17) {
+    console.log("child discount");
+  } else if (age <= 26) {
+    console.log("student discount");
+  } else if (age <= 66) {
+    console.log("full price");
+  } else {
+    console.log("senior citizen discount");
+  }
+}
+
+// ageCheck();
+
+let largeCountries = ["Tuvalu", "India", "USA", "Indonesia", "Monaco"];
+// console.log(largeCountries);
+largeCountries.shift();
+largeCountries.unshift("China");
+largeCountries.pop();
+largeCountries.push("Pakistan");
+// console.log(largeCountries);
+
+let hands = ["rock", "paper", "scissor"];
+function getHand() {
+  let randomIndex = Math.floor(Math.random() * 3);
+  return hands[randomIndex];
+}
+console.log(getHand());
--- test.js
+++
@@ -1,3 +0,0 @@
-// let num = Math.round(Math.random() * 6);
-let num = Math.floor(Math.random() * 6) + 1;
-console.log(num);
